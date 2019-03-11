cd640c52d89b1f197a2944c67b24eec1cce0e049
Constantinos Pigiotis <piyiotisk@gmail.com>
2019-03-11 22:15:15 +0000

    Add handlebars templating engine

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
+
+// view engine setup
+app.set('view engine', 'hbs');
+
+user = {
+    username: 'conpiy'
+}
+
+posts = [
+    {
+        'author': { 'username': 'alex' },
+        'content': `The PicoCoder's Node.js tutorial is cool.`
+    },
+    {
+        'author': { 'username': 'joanna' },
+        'content': 'I love javascript!'
+    }
+]
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.get('/', (req, res) => res.render('index', { title: 'PicoBlog', username: user.username, posts }));
+
 const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
 app.listen(port, () => console.log(`Server listening at: ${port}!`));
